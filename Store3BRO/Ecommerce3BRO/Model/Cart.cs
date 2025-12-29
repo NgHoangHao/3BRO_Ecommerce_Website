def6da99a4f34a7815739ec3d47db8a22b924684
NgHoangHao <haonguyenhoang8@gmail.com>
2025-12-29 09:20:41 +0700

    update

---
+++ Store3BRO/Ecommerce3BRO/Model/Cart.cs
@@ -0,0 +1,23 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Ecommerce3BRO.Model;
+
+[Table("Cart")]
+public partial class Cart
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    public Guid UserId { get; set; }
+
+    public DateTime? CreatedDate { get; set; }
+
+    
+    public  ICollection<CartItem>? CartItems { get; set; }
+
+    [ForeignKey("UserId")]
+    public virtual User? User { get; set; }
+}
